7c5462a3a86d34c79977a5076c67da7e24a08ffb
Dedrich <Dedrich@truepotential.ca>
2023-05-23 20:15:57 -0400

    Update

--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,4 @@
 # Here you can edit settings
 
 # The root save path
-saveDir = "Z:/True Potential Dropbox/Share/Houdini_Projects/_WOWLIB/_WMV_Characters"
+saveDir = "Z:\True Potential Dropbox\Share\Houdini_Projects\_WOWLIB\_WMV_Characters\_documentry\Trade District"
